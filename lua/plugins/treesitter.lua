4ad55cd80b7ed26ad1a22cae6902bba718577dfb
novel2430 <novel2430@163.com>
2026-04-07 00:39:48 +0800

    fix nvim treesitter bug

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,10 @@ local need_filetype = {
   'javascript',
   'lua',
   'markdown',
+  'markdown_inline',
+  'query',
   'vim',
+  'vimdoc',
   'python',
   'cpp',
   'java',
@@ -20,20 +23,27 @@ local need_filetype = {
   'nix',
 }
 
-require 'nvim-treesitter.configs'.setup {
+require('nvim-treesitter').setup {
   -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-  parser_install_dir = vim.fn.stdpath('data') .. '/site',
-  ensure_installed = need_filetype,
-  auto_install = true,
-  sync_install = false,
-  ignore_install = {},
-  highlight = { enable = true },
-  incremental_selection = { enable = true },
-  textobjects = { enable = true },
+  install_dir = vim.fn.stdpath('data') .. '/site'
 }
+
+local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+local parsersToInstall = vim.iter(need_filetype)
+    :filter(function(parser)
+      return not vim.tbl_contains(alreadyInstalled, parser)
+    end)
+    :totable()
+require('nvim-treesitter').install(parsersToInstall)
+
 
 -- Auto Enable Highlight
 vim.api.nvim_create_autocmd('FileType', {
   pattern = need_filetype,
-  callback = function() vim.treesitter.start() end,
+  callback = function()
+    -- Enable treesitter highlighting and disable regex syntax
+    pcall(vim.treesitter.start)
+    -- Enable treesitter-based indentation
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
 })
